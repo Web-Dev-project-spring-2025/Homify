fcbc0457441b67bd2b67d23491365649f4eed09d
Riana <riana.muktar06@mail.ru>
2025-03-23 09:35:47 +0500

    added data folder

---
+++ src/app/data/products.ts
@@ -0,0 +1,13 @@
+import { Product } from '../models';
+
+export const PRODUCTS: Product[] = [
+  {
+    id: 1,
+    name: 'ЛОСЬОН ДЛЯ ТЕЛА WHITE LOTUS',
+    description: 'Ароматизированный крем для рук и тела. Делает кожу мягкой и увлажненной и придает ей легкое благоухание.',
+    category: 'Aromas',
+    size: '500 ml',
+    price: 11980,
+    image: "https://static.zarahome.net/assets/public/2412/449a/820b4f8f8c04/7e96969107a2/40423701601-a7/40423701601-a7.jpg?ts=1731490509812&f=auto&w=1536"
+  },
+];
