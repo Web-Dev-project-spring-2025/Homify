964fed0d5a06e05ed4eceb0f065cc0344ea320b7
riririririiiiii <145469004+riririririiiiii@users.noreply.github.com>
2025-03-23 23:35:53 +0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
   standalone: true,
   imports: [RouterModule, ProductListComponent]
 })
