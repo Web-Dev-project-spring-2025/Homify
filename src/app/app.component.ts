8edc3801a0b1ad7d12c295264350790b021b5870
ayalanur_ <156872566+oceandrive8@users.noreply.github.com>
2025-03-25 17:39:59 +0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,29 +2,37 @@ import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { LoginComponent } from './login/login.component';
 import { ProductListComponent } from './product-list/product-list.component';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { HelpPopupComponent } from './help-popup/help-popup.component';
+import { NgIf } from '@angular/common';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [RouterModule, ProductListComponent]
+  imports: [RouterModule, ProductListComponent, MatDialogModule, NgIf]
 })
 export class AppComponent {
-  constructor(private dialog: MatDialog) {}
+  constructor(private dialog: MatDialog, private router: Router) {}
 
   openLogin() {
     this.dialog.open(LoginComponent, {
       width: '400px'
     });
   }
+
   openHelp() {
     this.dialog.open(HelpPopupComponent, {
       width: '400px'
     });
   }
-  
+
+  searchProducts(event: any, searchQuery: string) {
+    if (event.key === 'Enter' && searchQuery.trim()) {
+      this.router.navigate(['/search', searchQuery]);
+    }
+  } 
 }
 
