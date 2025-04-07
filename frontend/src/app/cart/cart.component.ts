e5c3e6f30f649456610a5f97ff7506319b0bb619
ayalanur_ <156872566+oceandrive8@users.noreply.github.com>
2025-04-07 20:36:41 +0500

    Update cart.component.ts

--- frontend/src/app/cart/cart.component.ts
+++ frontend/src/app/cart/cart.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartService, CartItem } from '../services/cart.service';
 import { Router } from '@angular/router';
-
+import { LoginComponent } from '../login/login.component';
+import { MatDialog } from '@angular/material/dialog';
 @Component({
   selector: 'app-cart',
   standalone: true,
@@ -14,7 +15,7 @@ import { Router } from '@angular/router';
 export class CartComponent {
   cartItems: CartItem[] = [];
 
-  constructor(private cartService: CartService, private router: Router) {
+  constructor(private cartService: CartService, private router: Router,private dialog: MatDialog ) {
     this.cartService.cart$.subscribe(items => {
       this.cartItems = items;
       console.log('Cart Items:', this.cartItems);
@@ -38,7 +39,16 @@ export class CartComponent {
   }
   
   navToAccount() {
-    this.router.navigate(['/profile']);
-  }
+      const user = localStorage.getItem('user');
+      if (user) {
+        this.router.navigate(['/profile']);
+      } else {
+        this.dialog.open(LoginComponent, {
+          width: '400px',
+          disableClose: true,
+        });
+      }
+    }
 
 }
+
