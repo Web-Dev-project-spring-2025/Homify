5911bed6b86c9030d41478b88466b077b6efcaac
ayalanur_ <156872566+oceandrive8@users.noreply.github.com>
2025-04-20 21:26:46 +0500

    Update cart.module.ts

--- frontend/src/app/cart/cart.module.ts
+++ frontend/src/app/cart/cart.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartComponent } from './cart.component';
-import { FormsModule } from '@angular/forms';  // Импортируем FormsMod
+import { FormsModule } from '@angular/forms'; 
 
 @NgModule({
   declarations: [CartComponent],
   imports: [
     CommonModule,
-    FormsModule  // Добавляем FormsModule сюда
+    FormsModule  
   ],
   exports: [CartComponent]
 })
